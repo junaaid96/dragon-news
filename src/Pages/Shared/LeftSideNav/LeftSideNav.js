16e83c3398984789d6a7f4f190add4ac8a5bde07
junaaid96 <info.junaidulislam@gmail.com>
2022-10-27 22:27:03 +0600

    ...

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -6,7 +6,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/categories')
+        fetch('https://dragon-news-server-lemon-three.vercel.app/categories')
             .then(res => res.json())
             .then(data => setCategories(data))
     }, []);
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -12,17 +12,17 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://dragon-news-server-lemon-three.vercel.app/news')
             },
             {
                 path: '/news/:id',
                 element: <News></News>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({ params }) => fetch(`https://dragon-news-server-lemon-three.vercel.app/news/${params.id}`)
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({ params }) => fetch(`https://dragon-news-server-lemon-three.vercel.app/category/${params.id}`)
             }
         ],
     },
